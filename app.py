8afff188309f8f9dfb8200eb283af76f9d4540c9
Anas Awadalla <anasa2@uw.edu>
2021-12-14 19:27:56 -0800

    Commiting to collab

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from collections import defaultdict
 import logging
+from os import read
 import sys
 from random import randint
 
 import numpy as np
 from flask import Flask, render_template, request
 from flask_socketio import ConnectionRefusedError, SocketIO, emit
+import tensorflow as tf
 
 from model import mnist_model
 
@@ -27,28 +29,28 @@ gradients_queue = []
 
 def average_gradients():
     global gradients_queue, parameters
-    if len(gradients_queue) < 5:
+    if len(gradients_queue) < 1:
         # don't do anything if too few gradients
         return False
 
-    model_gradients = {}
-    # TODO: update to factor in multiple layers
+    model_gradients = []
     for client_gradient in gradients_queue:
-        for layer in client_gradient: # Sum up all the gradients
-            if layer in model_gradients:
-                model_gradients[layer] += client_gradient[layer]
+        print(f"Client Keys: {client_gradient.keys()}")
+        for i, layer in enumerate(client_gradient): # Sum up all the gradients
+            if i < len(model_gradients):
+                model_gradients[i] += client_gradient[layer]
             else:
-                model_gradients[layer] = client_gradient[layer]
+                model_gradients.append(client_gradient[layer])
 
-    for layer in model_gradients: # Average all the gradients
-        model_gradients[layer] /= len(gradients_queue)
-
-    # avg = np.mean(gradients_queue, axis=0).astype(int).tolist()    
-    # parameters = (parameters + avg).astype(int)
-
-    # model.update_weights(model_gradients)
+    for i, _ in enumerate(model_gradients): # Average all the gradients
+        model_gradients[i] /= len(gradients_queue)
+        model_gradients[i] = tf.tensor(model_gradients[i])
     
-    serverlog(f'Averaged {len(gradients_queue)} gradients') # into {parameters}')
+    serverlog('Average gradients: {}'.format(model_gradients))
+    
+    model.update_weights(model_gradients)
+
+    serverlog(f'Averaged {len(gradients_queue)} gradients')
     gradients_queue = []
     return True
 
@@ -59,10 +61,13 @@ def serverlog(content):
     print(content)
 
 def send_parameters():
-    serverlog(f'Sent updated parameters {parameters.tolist()} to all clients')
-    socketio.emit('parameter update', {
-        'parameters': parameters.tolist()
+    # TODO: Send file to client by extracting weights from model and puting in a dictionary layerwise
+    model_parameters = model.get_weights()
+    emit('parameter update', {
+        'parameters': model_parameters
     })
+    serverlog('Sent updated parameters to all clients')
+
 
 @app.route('/')
 def index():
@@ -76,7 +81,7 @@ def receive_client_gradient(message):
     gradients = message['gradients']
     uuid = sidToUuid.get(request.sid, 'Unknown')
     gradients_queue.append(gradients)
-    serverlog(f'Received gradients {gradients} from {uuid} Queue length: {len(gradients_queue)}')
+    serverlog(f'Received gradients from {uuid} Queue length: {len(gradients_queue)}')
 
     if average_gradients():
         send_parameters()
@@ -91,9 +96,7 @@ def connect(auth):
     sidToUuid[request.sid] = uuid
     serverlog(f'Connected {uuid} Clients: {num_connections}')
     serverlog(f'Sent current parameters to {uuid}')
-    emit('parameter update', {
-        'parameters': parameters.tolist()
-    })
+    send_parameters()
 
 @socketio.on('disconnect')
 def test_disconnect():
@@ -109,4 +112,4 @@ if __name__ == '__main__':
     except IndexError:
         PORT = 8080
     print(f'Listening on port {PORT} on {HOST}')
-    socketio.run(app, host=HOST, port=PORT)
+    socketio.run(app, host=HOST, port=PORT, use_reloader=True)
--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import tensorflowjs as tfjs
-
+import numpy as np
 class mnist_model():
     """
     Model for MNIST
@@ -15,10 +15,20 @@ class mnist_model():
         self.model = keras.Model(inputs=inputs, outputs=outputs)
 
         self.optimizer = keras.optimizers.SGD(learning_rate=1e-3)
-        self.loss_func = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        # self.loss_func = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
     def update_weights(self, avg_gradient):
         self.optimizer.apply_gradients(zip(avg_gradient, self.model.trainable_weights))
     
     def save_weights(self, path):
-            tfjs.converters.save_keras_model(this.model, path)
+            tfjs.converters.save_keras_model(self.model, path)
+
+    def get_weights(self):
+        # TODO: return weights in a map where keys are layer names and values are arrays of weights
+        weights = {}
+        for layer in self.model.layers:
+            # iterate through kernels and bias weights
+            for weight in layer.weights:
+                weights[weight.name] = weight.numpy().tolist()
+        print(weights.keys())
+        return weights
